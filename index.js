e76be0a8a0317a0d9498e6a75e0bcc5c752d467e
gibsonbett <gibson.bett@student.moringaschool.com>
2022-08-18 02:40:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 62
+let num2 = 1
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() *5) +1;
+
+let num3 = 15
+let num4 = 11
+let mod = num3 % num4;
+
+let max = Math.max(0,20)
